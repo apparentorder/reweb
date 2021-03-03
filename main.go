1adf2136ff61d4e84d00a2bfba3b2a55fecc9877
apparentorder <github@neveragain.de>
2021-03-03 18:51:56 +0100

    rework a few bits: - invoke our handler via StartHandler() instead,   to support generic / multiple payloads - support ALB request payloads (events.ALBTargetGroupRequest),   and take care to treat headers correctly - error handling: mor checks, more uniform output - on cold-start: check for local web server every 50ms (down from 100ms) - add var Version to record build timestamp (via Makefile) - cosmetic fixes

--- main.go
+++ main.go
@@ -5,7 +5,8 @@ import (
 	"context"
 	"compress/gzip"
 	"encoding/base64"
-	"errors"
+	"encoding/json"
+	//"errors"
 	"io/ioutil"
 	"fmt"
 	"net/http"
@@ -17,6 +18,8 @@ import (
 	"github.com/aws/aws-lambda-go/events"
 )
 
+var Version string
+
 var ApplicationPort string
 var ApplicationExec string
 var ForceGzip bool
@@ -24,88 +27,199 @@ var WaitCode string
 var WaitPath string
 var Debug bool
 
+const PrefixDebug = "re:Web -- debug:  "
+const PrefixInfo  = "re:Web -- "
+const PrefixError = "re:Web -- ERROR:  "
+
 func debug(s string) {
 	if Debug {
-		fmt.Printf(s + "\n")
+		fmt.Printf("%s%s\n", PrefixDebug, s)
 	}
 }
 
-func HandleRequest(ctx context.Context, event events.APIGatewayV2HTTPRequest) (events.APIGatewayV2HTTPResponse, error) {
-	defer func() {
-		if err := recover(); err != nil {
-			fmt.Println("RE:WEB PANIC:", err)
+type lambdaHandler struct {}
 
-			if Debug {
-				exec := syscall.ProcAttr{
-					Env: os.Environ(),
-					Files: []uintptr{os.Stdin.Fd(), os.Stdout.Fd(), os.Stderr.Fd()},
-				}
+func (h lambdaHandler) Invoke(ctx context.Context, payload []byte) ([]byte, error) {
+	var isALB = false
 
-				syscall.ForkExec("/bin/sh", []string{"/bin/sh", "-c", "netstat -plne 2>&1"}, &exec)
-				syscall.ForkExec("/bin/sh", []string{"/bin/sh", "-c", "ps axufw 2>&1"}, &exec)
-			}
+	var err error
+	var albRequest events.ALBTargetGroupRequest
+	var apiGwRequest events.APIGatewayV2HTTPRequest
 
-			os.Exit(69)
+	type Event struct {
+		Body string
+		IsBase64Encoded bool
+		Path string
+		Headers map[string]string
+		HTTPMethod string
+	}
+	var event Event
+
+	debug("=== Invoke =============================================================")
+
+	//debug("EVENT:")
+	//debug(string(payload))
+
+	err = json.NewDecoder(bytes.NewReader(payload)).Decode(&apiGwRequest)
+	if err != nil {
+		e := fmt.Errorf(PrefixError + "cannot parse payload as API Gateway request: %v", err)
+		fmt.Println(e)
+		return nil, e
+	}
+
+	if apiGwRequest.RequestContext.APIID != "" {
+		// if an API ID is present, we can be sure it was an API Gateway request
+		isALB = false
+		debug(fmt.Sprintf("Parsed as API Gateway request"))
+		//debug(fmt.Sprintf("%+v", apiGwRequest))
+
+		event = Event{
+			Body: apiGwRequest.Body,
+			IsBase64Encoded: apiGwRequest.IsBase64Encoded,
+			Path: apiGwRequest.RequestContext.HTTP.Path,
+			HTTPMethod: apiGwRequest.RequestContext.HTTP.Method,
 		}
-	}()
+	} else {
+		// otherwise, we'll assume we were not invoked via API Gateway but via ALB instead.
+		isALB = true
 
-	debug(fmt.Sprintf("EVENT OBJECT: %+v", event))
+		err = json.NewDecoder(bytes.NewReader(payload)).Decode(&albRequest)
+		if err != nil {
+			e := fmt.Errorf(PrefixError + "cannot parse payload as ALB  request: %v", err)
+			fmt.Println(e)
+			return nil, e
+		}
+		debug(fmt.Sprintf("Parsed as ALB request"))
+		debug(fmt.Sprintf("%+v", albRequest))
 
-	// do not follow redirects;
-	// https://stackoverflow.com/questions/23297520/how-can-i-make-the-go-http-client-not-follow-redirects-automatically
-	client := &http.Client{
+		event = Event{
+			Body: albRequest.Body,
+			IsBase64Encoded: albRequest.IsBase64Encoded,
+			Path: albRequest.Path,
+			HTTPMethod: albRequest.HTTPMethod,
+		}
+	}
+
+	debug("--- Build Request ------------------------------------------------------")
+
+	var body []byte
+	if event.IsBase64Encoded {
+		body, err = base64.StdEncoding.DecodeString(event.Body)
+		if err != nil {
+			e := fmt.Errorf(PrefixError + "cannot base64-decode request body: %v", err)
+			fmt.Println(e)
+			return nil, e
+		}
+	} else {
+		body = []byte(event.Body)
+	}
+
+	path := event.Path
+	debug("PATH: " + path)
+
+	if !isALB {
+		if apiGwRequest.RawQueryString != "" {
+			debug("PATH NEEDS QS")
+			path += "?" + apiGwRequest.RawQueryString
+			debug("PATH POST QS: " + path)
+		}
+	} else {
+		if len(albRequest.MultiValueQueryStringParameters) > 0 {
+			debug("PATH NEEDS QS")
+			kv := []string{}
+			for mvqsp, v := range albRequest.MultiValueQueryStringParameters {
+				for _, s := range v {
+					kv = append(kv, mvqsp + "=" + s)
+				}
+			}
+			path += "?" + strings.Join(kv, "&")
+			debug("PATH POST QS: " + path)
+		}
+	}
+
+	req, err := http.NewRequest(
+		event.HTTPMethod,
+		"http://localhost:" + ApplicationPort + path,
+		bytes.NewReader(body),
+	)
+
+	if err != nil {
+		e := fmt.Errorf(PrefixError + "http.NewRequest: %v", err)
+		fmt.Println(e)
+		return nil, e
+	}
+
+	debug(fmt.Sprintf("REQUEST: %s %s\n", req.Method, req.URL))
+
+	var host string
+	if isALB {
+		// ALB: Payload *must* use MultiValueHeaders, because this can only be configured to
+		// to MVHeaders exclusively -- for Request AND Response -- or not at all.
+		// Unfortunately, "not at all" does not work with Cookies at all, because returning
+		// multiple Cookies (Set-Cookie headers) cannot work without MVHeaders.
+
+		if len(albRequest.Headers) > 0 {
+			e := fmt.Errorf(PrefixError + "ALB payload without MultiValueHeaders -- check the ALB Target Group settings!")
+			fmt.Println(e)
+			return nil, e
+		}
+
+		for h, v := range albRequest.MultiValueHeaders {
+			// as the net/http docs say: "To use non-canonical keys, assign to the map directly." :E
+			// apparently, this also applies to multi-value headers.
+			req.Header[http.CanonicalHeaderKey(h)] = v
+		}
+
+		if h, ok := albRequest.MultiValueHeaders["host"]; !ok || len(h) != 1 {
+			e := fmt.Errorf(PrefixError + "ALB payload with bad 'Host' header")
+			fmt.Println(e)
+			return nil, e
+		}
+		host = albRequest.MultiValueHeaders["host"][0]
+	} else {
+		// API Gateway: V2 payload *never* uses MultiValue{Headers,QS}, so everything
+		// is in .Headers as usual -- except for Cookies, of course.
+		for h, v := range apiGwRequest.Headers {
+			req.Header.Set(h, v)
+		}
+
+		// set Cookie header from event.Cookies (it gets eaten by API Gateway and stored there)
+		req.Header.Set("Cookie", strings.Join(apiGwRequest.Cookies, "; "))
+
+		host = req.Header.Get("Host")
+	}
+
+	// net/http will set/overwrite Host: using the destination address (localhost!), even if
+	// it is explicitly present in req.Header. the workaround is to set request.Host instead.
+	req.Host = host
+
+	debug(fmt.Sprintf("REQUEST OBJECT: %+v", req))
+
+	debug("--- Send Request -------------------------------------------------------")
+
+	httpClient := &http.Client{
 		CheckRedirect: func(req *http.Request, via []*http.Request) error {
 			return http.ErrUseLastResponse
 		},
 	}
 
-	var body []byte
-	if event.IsBase64Encoded {
-		body, _ = base64.StdEncoding.DecodeString(event.Body)
-	} else {
-		body = []byte(event.Body)
-	}
-
-	path := event.RequestContext.HTTP.Path
-	debug("PATH: " + path)
-	if event.RawQueryString != "" {
-		debug("PATH NEEDS QS")
-		path += "?" + event.RawQueryString
-		debug("PATH POST QS: " + path)
-	}
-
-	req, _ := http.NewRequest(
-		event.RequestContext.HTTP.Method,
-		"http://localhost:" + ApplicationPort + path,
-		bytes.NewReader(body),
-	)
-
-	debug(fmt.Sprintf("REQUEST: %s %s\n", req.Method, req.URL))
-
-	// set request headers; can't assign all of them at once because
-	// Request.Header values are []string instead of string.
-	for h, v := range event.Headers {
-		req.Header.Set(h, v)
-	}
-
-	// set Cookie header. it gets eaten by API Gateway and stored in event.Cookies instead.
-	req.Header.Set("Cookie", strings.Join(event.Cookies, "; "))
-
-	// net/http will set/overwrite Host: using the destination address (localhost!), even if
-	// it is explicitly present in req.Header. the workaround is to set request.Host instead.
-	req.Host = event.Headers["host"]
-
-	debug(fmt.Sprintf("REQUEST OBJECT: %+v", req))
-
-	resp, err := client.Do(req)
+	resp, err := httpClient.Do(req)
 	if err != nil {
-		fmt.Println(fmt.Sprintf("HTTP REQUEST FAILED: %v", err))
-		return events.APIGatewayV2HTTPResponse{}, errors.New(fmt.Sprintf("HTTP REQUEST FAILED: %v", err))
+		e := fmt.Errorf(PrefixError + "HTTP REQUEST FAILED: %v", err)
+		fmt.Println(e)
+		return nil, e
 	}
 	debug("RESPONSE: " + resp.Status)
 
+	debug("--- Build Response -----------------------------------------------------")
+
 	var respBody string
-	respBodyBytes, _ := ioutil.ReadAll(resp.Body)
+	respBodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		e := fmt.Errorf(PrefixError + "ReadAll() on response body: %v", err)
+		fmt.Println(e)
+		return nil, e
+	}
 
 	if !ForceGzip {
 		respBody = base64.StdEncoding.EncodeToString(respBodyBytes)
@@ -117,28 +231,55 @@ func HandleRequest(ctx context.Context, event events.APIGatewayV2HTTPRequest) (e
 		respBody = base64.StdEncoding.EncodeToString(respBodyGz.Bytes())
 	}
 
-	r := events.APIGatewayV2HTTPResponse{
-		StatusCode: resp.StatusCode,
-		Body: respBody,
-		IsBase64Encoded: true,
-	}
+	var responseHeaders = map[string]string{}
+	var responseCookies = []string{}
 
-	r.Headers = map[string]string{}
 	for h, v := range resp.Header {
 		if h == "Set-Cookie" {
 			for _, s := range v {
-				r.Cookies = append(r.Cookies, s)
+				responseCookies = append(responseCookies, s)
 			}
 		} else {
-			r.Headers[h] = strings.Join(v, ", ")
+			responseHeaders[h] = strings.Join(v, ", ")
 		}
 	}
 
 	if ForceGzip {
-		r.Headers["Content-Encoding"] = "gzip"
+		responseHeaders["Content-Encoding"] = "gzip"
 	}
 
-	return r, nil
+	if !isALB {
+		r := events.APIGatewayV2HTTPResponse{
+			StatusCode: resp.StatusCode,
+			Body: respBody,
+			IsBase64Encoded: true,
+			Headers: responseHeaders,
+			Cookies: responseCookies,
+		}
+		jsonResponse, _ := json.Marshal(r)
+		debug(string(jsonResponse))
+		return jsonResponse, nil
+	} else {
+		r := events.ALBTargetGroupResponse{
+			StatusCode: resp.StatusCode,
+			Body: respBody,
+			IsBase64Encoded: true,
+			MultiValueHeaders: map[string][]string{},
+		}
+
+		for k, v := range responseHeaders {
+			r.MultiValueHeaders[k] = append(r.MultiValueHeaders[k], v)
+		}
+
+		for _, s := range responseCookies {
+			debug("Adding cookie: " + s)
+			r.MultiValueHeaders["Set-Cookie"] = append(r.MultiValueHeaders["Set-Cookie"], s)
+		}
+
+		jsonResponse, _ := json.Marshal(r)
+		debug(string(jsonResponse))
+		return jsonResponse, nil
+	}
 }
 
 func main() {
@@ -147,11 +288,19 @@ func main() {
 	ForceGzip = (os.Getenv("REWEB_FORCE_GZIP") != "")
 	WaitPath = os.Getenv("REWEB_WAIT_PATH")
 	WaitCode = os.Getenv("REWEB_WAIT_CODE")
+	Debug = (os.Getenv("REWEB_DEBUG") != "")
 
 	if WaitPath == "" { WaitPath = "/" }
 
 	if ApplicationExec == "" { panic("Missing REWEB_APPLICATION_EXEC environment variable") }
 	if ApplicationPort == "" { panic("Missing REWEB_APPLICATION_PORT environment variable") }
+
+	debug("Version: " + Version)
+
+	debug("ENVIRONMENT:")
+	for _, e := range os.Environ() {
+		debug("         ... " + e)
+	}
 
 	exec := syscall.ProcAttr{
 		Env: os.Environ(),
@@ -162,38 +311,40 @@ func main() {
 
 	tries := 0
 	for true {
-		c := &http.Client{
+		httpClient := &http.Client{
 			Timeout: 2 * time.Second,
 			CheckRedirect: func(req *http.Request, via []*http.Request) error {
 				return http.ErrUseLastResponse
 			},
 		}
-		resp, err := c.Get("http://localhost:" + ApplicationPort + WaitPath)
+
+		resp, err := httpClient.Get("http://localhost:" + ApplicationPort + WaitPath)
 		if err != nil {
 			if tries % 10 == 0 {
-				fmt.Printf("SERVICE NOT UP: %v\n", err)
+				fmt.Printf(PrefixInfo + "SERVICE NOT UP: %v\n", err)
 			}
 		} else {
 			if WaitCode == "" || fmt.Sprint(resp.StatusCode) == WaitCode {
-				fmt.Printf("SERVICE UP: %s\n", resp.Status)
+				fmt.Printf(PrefixInfo + "SERVICE UP: %s\n", resp.Status)
 				break
 			}
 
 			status := resp.Status
 			if resp.StatusCode == 302 {
-				status = fmt.Sprintf("%s, Location: %s", resp.Status,
+				status = fmt.Sprintf(PrefixInfo + "%s, Location: %s", resp.Status,
 					resp.Header.Get("Location"))
 			}
 
-			fmt.Printf("SERVICE UP, NOT READY: Expecting HTTP %s, got: %s\n",
+			fmt.Printf(PrefixInfo + "SERVICE UP, NOT READY: Expecting HTTP %s, got: %s\n",
 				WaitCode, status)
 		}
 
-		time.Sleep(100 * time.Millisecond)
+		time.Sleep(50 * time.Millisecond)
 
 		tries += 1
 	}
 
-	lambda.Start(HandleRequest)
+	lambda.StartHandler(lambdaHandler{})
+	// UNREACH
 }
 
